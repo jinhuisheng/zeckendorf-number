dfb9eb095c3a8c86314655b02f8b97fc7f334cc7
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-21 21:48:58 +0800

    empty message

--- src/test/java/zeckendorf/ZeckendorfRepresentationTest.java
+++ src/test/java/zeckendorf/ZeckendorfRepresentationTest.java
@@ -9,9 +9,17 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class ZeckendorfRepresentationTest {
     @Test
-    void generate_0() {
+    void generate_from_0_to_0() {
         ZeckendorfRepresentation zeckendorfRepresentation = new ZeckendorfRepresentation(0, 0);
         List<Integer> result = zeckendorfRepresentation.generate();
         assertThat(result).isEqualTo(Arrays.asList(0));
     }
+
+    @Test
+    void generate_from_0_to_1() {
+        ZeckendorfRepresentation zeckendorfRepresentation = new ZeckendorfRepresentation(0, 0);
+        List<Integer> result = zeckendorfRepresentation.generate();
+        assertThat(result).isEqualTo(Arrays.asList(0));
+    }
+
 }
