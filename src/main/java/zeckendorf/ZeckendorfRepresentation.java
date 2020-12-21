cbf98cb910eae2b6f88736dc68b91996d6133456
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-21 21:44:12 +0800

    empty message

--- src/main/java/zeckendorf/ZeckendorfRepresentation.java
+++ src/main/java/zeckendorf/ZeckendorfRepresentation.java
@@ -4,4 +4,8 @@ public class ZeckendorfRepresentation {
     public ZeckendorfRepresentation(int start, int end) {
 
     }
+
+    public Integer generate() {
+        return 0;
+    }
 }
--- src/test/java/zeckendorf/ZeckendorfRepresentationTest.java
+++ src/test/java/zeckendorf/ZeckendorfRepresentationTest.java
@@ -2,10 +2,13 @@ package zeckendorf;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class ZeckendorfRepresentationTest {
     @Test
     void generate_0() {
         ZeckendorfRepresentation zeckendorfRepresentation = new ZeckendorfRepresentation(0, 0);
-        
+        Integer result = zeckendorfRepresentation.generate();
+        assertThat(result).isEqualTo(0);
     }
 }
