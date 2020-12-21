ce68a9d734a794165c35dd5b754195b6fd10741a
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-21 21:47:21 +0800

    empty message

--- src/main/java/zeckendorf/ZeckendorfRepresentation.java
+++ src/main/java/zeckendorf/ZeckendorfRepresentation.java
@@ -2,13 +2,24 @@ package zeckendorf;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ZeckendorfRepresentation {
+    private final int start;
+    private final int end;
+
     public ZeckendorfRepresentation(int start, int end) {
 
+        this.start = start;
+        this.end = end;
     }
 
     public List<Integer> generate() {
-        return Arrays.asList(0);
+        return IntStream.range(start, end + 1).map(i -> generate(i)).boxed().collect(Collectors.toList());
+    }
+
+    private int generate(int number) {
+        return 0;
     }
 }
