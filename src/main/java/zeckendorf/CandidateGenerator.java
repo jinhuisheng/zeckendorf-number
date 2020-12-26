5ba76014c1ef03f5b66c8754f860663c9e15eafd
Seaborn Lee <seabornlee.cn@gmail.com>
2020-12-26 11:07:54 +0800

    test: generate two

--- src/main/java/zeckendorf/CandidateGenerator.java
+++ src/main/java/zeckendorf/CandidateGenerator.java
@@ -1,9 +1,10 @@
 package zeckendorf;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CandidateGenerator {
-    public List<Integer> generate(int count) {
-        return null;
+    public List<List<Integer>> generate(int count) {
+        return Arrays.asList(Arrays.asList(0), Arrays.asList(1));
     }
 }
--- src/test/java/zeckendorf/CandidateGeneratorTest.java
+++ src/test/java/zeckendorf/CandidateGeneratorTest.java
@@ -2,6 +2,7 @@ package zeckendorf;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +12,20 @@ public class CandidateGeneratorTest {
     @Test
     void generate_one() {
         CandidateGenerator candidateGenerator = new CandidateGenerator();
-        List<Integer> result = candidateGenerator.generate(1);
-        assertThat(result).isEqualTo(Arrays.asList(0, 1));
+        List<List<Integer>> result = candidateGenerator.generate(1);
+        assertThat(result).isEqualTo(Arrays.asList(Arrays.asList(0), Arrays.asList(1)));
     }
+
+    @Test
+    void generate_two() {
+        CandidateGenerator candidateGenerator = new CandidateGenerator();
+        List<List<Integer>> result = candidateGenerator.generate(2);
+        assertThat(result).isEqualTo(Arrays.asList(
+                Arrays.asList(1, 1),
+                Arrays.asList(1, 0),
+                Arrays.asList(0, 1),
+                Arrays.asList(0, 0)
+        ));
+    }
+
 }
