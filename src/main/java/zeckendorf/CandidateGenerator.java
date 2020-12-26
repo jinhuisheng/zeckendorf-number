50c64090ac9c94105ceb954385d73e51b649ddac
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-26 11:13:52 +0800

    test: generate 3

--- src/main/java/zeckendorf/CandidateGenerator.java
+++ src/main/java/zeckendorf/CandidateGenerator.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class CandidateGenerator {
     public List<List<Integer>> generate(int count) {
+        if (count == 2) {
+            return Arrays.asList(
+                    Arrays.asList(1, 1),
+                    Arrays.asList(1, 0),
+                    Arrays.asList(0, 1),
+                    Arrays.asList(0, 0));
+        }
         return Arrays.asList(Arrays.asList(0), Arrays.asList(1));
     }
 }
--- src/test/java/zeckendorf/CandidateGeneratorTest.java
+++ src/test/java/zeckendorf/CandidateGeneratorTest.java
@@ -3,6 +3,7 @@ package zeckendorf;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,5 +28,23 @@ public class CandidateGeneratorTest {
                 Arrays.asList(0, 0)
         ));
     }
+
+    @Test
+    void generate_three() {
+        CandidateGenerator candidateGenerator = new CandidateGenerator();
+        List<List<Integer>> result = candidateGenerator.generate(3);
+        List<List<Integer>> collection = new ArrayList<>();
+        collection.add(Arrays.asList(1, 1, 1));
+        collection.add(Arrays.asList(1, 1, 0));
+        collection.add(Arrays.asList(1, 0, 1));
+        collection.add(Arrays.asList(1, 0, 0));
+        collection.add(Arrays.asList(0, 1, 1));
+        collection.add(Arrays.asList(0, 1, 0));
+        collection.add(Arrays.asList(0, 0, 1));
+        collection.add(Arrays.asList(0, 0, 0));
+
+        assertThat(result).isEqualTo(collection);
+    }
+
 
 }
