ce81a05a863f03aa14e53a07e8a0964ac62d450b
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 18:53:57 +0800

    feat: complete candidateGenerator

--- src/main/java/zeckendorf/CandidateGenerator.java
+++ src/main/java/zeckendorf/CandidateGenerator.java
@@ -1,27 +1,22 @@
 package zeckendorf;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
 
 public class CandidateGenerator {
     public List<List<Integer>> generate(int count) {
         if (count == 1) {
-            return asList(
-                    singletonList(1),
-                    singletonList(0)
-            );
+            return Arrays.asList(Arrays.asList(1), Arrays.asList(0));
         }
-
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 1; i >= 0; i--) {
-            for (List<Integer> integers : generate(count - 1)) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(i);
-                temp.addAll(integers);
-                list.add(temp);
+        for (List<Integer> integers : generate(1)) {
+            List<List<Integer>> currentList = generate(count - 1);
+            for (List<Integer> integerList : currentList) {
+                List<Integer> item = new ArrayList<>();
+                item.add(integers.get(0));
+                item.addAll(integerList);
+                list.add(item);
             }
         }
         return list;
