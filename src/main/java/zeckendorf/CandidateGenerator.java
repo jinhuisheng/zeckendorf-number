40b3e331b9e5799e1c9cc7a10f3564c76bdeea35
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 21:27:51 +0800

    feat: tdd candidate again

--- src/main/java/zeckendorf/CandidateGenerator.java
+++ src/main/java/zeckendorf/CandidateGenerator.java
@@ -2,23 +2,24 @@ package zeckendorf;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class CandidateGenerator {
     public List<List<Integer>> generate(int count) {
         if (count == 1) {
-            return Arrays.asList(Arrays.asList(1), Arrays.asList(0));
+            return Arrays.asList(Collections.singletonList(1), Collections.singletonList(0));
         }
-        List<List<Integer>> list = new ArrayList<>();
-        for (List<Integer> integers : generate(1)) {
-            List<List<Integer>> currentList = generate(count - 1);
-            for (List<Integer> integerList : currentList) {
-                List<Integer> item = new ArrayList<>();
-                item.add(integers.get(0));
-                item.addAll(integerList);
-                list.add(item);
+        List<List<Integer>> candidates = new ArrayList<>();
+        List<List<Integer>> candidatesOfMinus1Digit = generate(count - 1);
+        for (int i = 1; i >= 0; i--) {
+            for (List<Integer> candidate : candidatesOfMinus1Digit) {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(i);
+                temp.addAll(candidate);
+                candidates.add(temp);
             }
         }
-        return list;
+        return candidates;
     }
 }
